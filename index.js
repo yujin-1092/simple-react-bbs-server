aeeef4c3eac5ae6d58aa7439afb59ef5ddb2f91b
yujin-1092 <kunche1092@gmail.com>
2024-11-07 09:54:29 +0900

    1

--- index.js
+++ index.js
@@ -3,47 +3,95 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+//본문을 통해서 넘어온 요청 파싱(변환) 미들웨어(body-parser)
+//본문을 통해서 넘어온 name=Alice&age=25
+app.use(express.json()); //json 형식으로 변환 { "name":"Alice", "age":"25"}
+app.use(express.urlencoded()); //json->object { name: "Alice", age: "25" } 
 
-var corsOption = {
-  origin : '*', //모든 출처 허용
-  optionsSuccessStatues:200
+var corsOptions = {
+  //origin: 'http://localhost:3000/',
+  origin: '*' //모든 출처 허용
 }
 
-app.use(cors(corsOption));
+app.use(cors(corsOptions));
 
-var mysql      = require('mysql');
-var db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'react_bbs',
-  password : '12345',
+const mysql = require('mysql')
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'react_bbs',
+  password: '12345',
   database: 'react_bbs'
-});
+})
 
-db.connect();
+db.connect()
 
-
+/*
 app.get('/', (req, res) => {
   const sql = "INSERT INTO requested (rowno) VALUES (1)";
-
-  db.query(sql, function(err, rows, fields) {
-    if (err) throw err;
+  db.query(sql, (err, rows, fields) => {
+    if (err) throw err;  
     res.send('성공');
-    console.log('데이터 추가 성공');
-  });
+    console.log('데이터 추가 성공')
+  })  
 })
+*/
 
-
-app.get('/', (req, res) => {
-  const sql = "SELECT * FROM board";
-
-  db.query(sql, function(err, result) {
-    if (err) throw err;
+app.get('/list', (req, res) => {
+  const sql = "SELECT BOARD_ID, BOARD_TITLE, REGISTER_ID, DATE_FORMAT(REGISTER_DATE , '%Y-%m-%d') AS REGISTER_DATE FROM board";
+  db.query(sql, (err, result) => {
+    if (err) throw err;  
     res.send(result);
-  });
+  })  
 })
+
+app.get('/detail', (req, res) => {
+  const id = req.query.id; 
+  const sql = "SELECT BOARD_TITLE, BOARD_CONTENT FROM board WHERE BOARD_ID = ?";
+  db.query(sql, [id],(err, result) => {
+    if (err) throw err;  
+    res.send(result);
+  })  
+})
+
+app.post('/insert', (req, res) => {
+
+  let title = req.body.title;
+  let content = req.body.content;
+  const sql = "INSERT INTO board (BOARD_TITLE, BOARD_CONTENT, REGISTER_ID) VALUES (?,?,'admin')";
+  db.query(sql, [title, content], (err, result) => {
+    if (err) throw err;  
+    res.send(result);
+  })  
+})
+
+app.post('/update', (req, res) => {
+
+  // let title = req.body.title;
+  // let content = req.body.content;
+  // let id = req.body.id;
+  const {id, title, content} = req.body;
+  
+  const sql = "UPDATE board  SET BOARD_TITLE=?, BOARD_CONTENT=? WHERE BOARD_ID=?";
+  db.query(sql, [title, content, id], (err, result) => {
+    if (err) throw err;  
+    res.send(result);
+  })  
+})
+
+app.post('/delete', (req, res) => {
+//const boardIDList = req.body.boardIDList;
+  const {boardIDList} = req.body;
+  
+  const sql = `DELETE from board where BOARD_ID in (${boardIDList})`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;  
+    res.send(result);
+  })  
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-// db.end();
+//db.end()
